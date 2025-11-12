86f2c1ae4002dfa473aef10a8bc763671a8189e9
avii778 <142438557+avii778@users.noreply.github.com>
2025-11-11 19:13:17 -0500

    Added some folders such as App, data_access, etc.

---
+++ src/main/java/View/LoginView.java
@@ -0,0 +1,20 @@
+package View;
+
+import interface_adapter.login.LoginController;
+import interface_adapter.login.LoginState;
+import interface_adapter.login.LoginViewModel;
+
+import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+public class LoginView {
+
+
+
+}
---
+++ src/main/java/data_access/FileUserDataAccessObject.java
@@ -0,0 +1,16 @@
+package data_access;
+
+import Entities.User;
+import entity.UserFactory;
+import use_case.change_password.ChangePasswordUserDataAccessInterface;
+import use_case.login.LoginUserDataAccessInterface;
+import use_case.logout.LogoutUserDataAccessInterface;
+import use_case.signup.SignupUserDataAccessInterface;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class FileUserDataAccessObject {
+}
