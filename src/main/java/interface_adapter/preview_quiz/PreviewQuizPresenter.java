a28c245d16d937fed62d80ee21eaeb049d3002b2
Felcita606 <michellezhou06@hotmail.com>
2025-11-14 01:25:12 -0500

    preview quiz adapter finished

--- src/main/java/interface_adapter/preview_quiz/PreviewQuizPresenter.java
+++ src/main/java/interface_adapter/preview_quiz/PreviewQuizPresenter.java
@@ -1,4 +1,38 @@
 package interface_adapter.preview_quiz;
 
-public class PreviewQuizPresenter {
+import interface_adapter.ViewManagerModel;
+import use_cases.preview_quiz.PreviewQuizOutputBoundary;
+import use_cases.preview_quiz.PreviewQuizOutputData;
+
+public class PreviewQuizPresenter implements PreviewQuizOutputBoundary{
+
+    private final PreviewQuizViewModel previewQuizViewModel;
+    private final ViewManagerModel viewManagerModel;
+
+    public PreviewQuizPresenter(ViewManagerModel viewManagerModel,
+                                PreviewQuizViewModel previewQuizViewModel) {
+        this.viewManagerModel = viewManagerModel;
+        this.previewQuizViewModel = previewQuizViewModel;
+    }
+
+
+    @Override
+    public void prepareSuccessView(PreviewQuizOutputData outputData) {
+        final PreviewQuizState previewQuizState = previewQuizViewModel.getState();
+        previewQuizState.setPreviewError(null);
+        previewQuizState.setQuestions(outputData.getQuestions());
+        previewQuizState.setCurrentQuestionIndex(0);
+
+        previewQuizViewModel.firePropertyChange();
+    }
+
+    @Override
+    public void prepareFailView(String previewError) {
+        PreviewQuizState previewQuizState = previewQuizViewModel.getState();
+        previewQuizState.setPreviewError(previewError);
+        previewQuizState.setQuestions(null);
+        previewQuizState.setCurrentQuestionIndex(0);
+
+        previewQuizViewModel.firePropertyChange();
+    }
 }
--- src/main/java/interface_adapter/preview_quiz/PreviewQuizViewModel.java
+++ src/main/java/interface_adapter/preview_quiz/PreviewQuizViewModel.java
@@ -1,4 +1,12 @@
 package interface_adapter.preview_quiz;
 
-public class PreviewQuizViewModel {
+import interface_adapter.ViewModel;
+import interface_adapter.login.LoginState;
+
+public class PreviewQuizViewModel extends ViewModel<PreviewQuizState> {
+
+    public PreviewQuizViewModel() {
+        super("preview quiz");
+        setState(new PreviewQuizState());
+    }
 }
