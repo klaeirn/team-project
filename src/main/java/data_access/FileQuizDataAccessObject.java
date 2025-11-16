3ea7f2e849bbc04e8dbe2240c883dad713457563
avii778 <142438557+avii778@users.noreply.github.com>
2025-11-16 00:35:41 -0500

    Added get quiz functionallity

--- src/main/java/data_access/FileQuizDataAccessObject.java
+++ src/main/java/data_access/FileQuizDataAccessObject.java
@@ -2,6 +2,7 @@ package data_access;
 
 import entities.Question;
 import entities.Quiz;
+import entities.QuizFactory;
 import org.json.JSONException;
 import use_cases.create_quiz.CreateQuizDataAccessInterface;
 import use_cases.create_quiz.UserDataAccessInterface;
@@ -10,8 +11,10 @@ import org.json.JSONArray;
 import org.json.JSONString;
 
 
-
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FileQuizDataAccessObject implements CreateQuizDataAccessInterface {
 
@@ -45,4 +48,53 @@ public class FileQuizDataAccessObject implements CreateQuizDataAccessInterface {
 
     }
 
+    public Quiz getQuiz(String quizName, String username) {
+        QuizFactory quizFactory = new QuizFactory();
+
+        try {
+            String jsonStr = Files.readString(quizPath);
+            JSONArray quizArray = new JSONArray(jsonStr);
+
+            for (int i = 0; i < quizArray.length(); i++) {
+                JSONObject quizJson = quizArray.getJSONObject(i);
+
+                String name = quizJson.getString("quizName");
+                String creator = quizJson.getString("quizCreator");
+
+                if (name.equals(quizName) && creator.equals(username)) {
+
+                    String category = quizJson.getString("category");
+
+                    JSONArray questionsJson = quizJson.getJSONArray("questions");
+                    List<Question> questionList = new ArrayList<>();
+
+                    for (int j = 0; j < questionsJson.length(); j++) {
+                        JSONObject qJson = questionsJson.getJSONObject(j);
+
+                        String questionName = qJson.getString("questionName");
+                        String answer = qJson.getString("answer");
+
+                        JSONArray optionsJson = qJson.getJSONArray("options");
+                        List<String> options = new ArrayList<>();
+
+                        for (int k = 0; k < optionsJson.length(); k++) {
+                            options.add(optionsJson.getString(k));
+                        }
+
+                        questionList.add(new Question(questionName, options, answer));
+                    }
+
+                    return quizFactory.createQuiz(name, creator, category, questionList);
+                }
+            }
+
+            return null;
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
 }
+
