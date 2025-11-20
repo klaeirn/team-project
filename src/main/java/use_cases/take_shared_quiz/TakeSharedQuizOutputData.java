392de059306cdb669781f8975735f47373a10e59
kelongrenrenren <liuyx1130@gmail.com>
2025-11-20 11:31:09 -0500

    added data access interface and boundary and input data for take shared quiz 2. added the rest of the use case

--- src/main/java/use_cases/take_shared_quiz/TakeSharedQuizOutputData.java
+++ src/main/java/use_cases/take_shared_quiz/TakeSharedQuizOutputData.java
@@ -1,4 +1,14 @@
 package use_cases.take_shared_quiz;
 
+import entities.Quiz;
+
 public class TakeSharedQuizOutputData {
+    private final Quiz quiz;
+
+    public TakeSharedQuizOutputData(Quiz quiz) {
+        this.quiz = quiz;
+    }
+    public Quiz getQuiz() {
+        return quiz;
+    }
 }
