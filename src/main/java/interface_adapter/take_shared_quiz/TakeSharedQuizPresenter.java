292c3c06a2463afeacf620a160c130c0d7cae3be
kelongrenrenren <liuyx1130@gmail.com>
2025-11-21 06:53:57 -0500

    presenter, controller, state, viewmodel

--- src/main/java/interface_adapter/take_shared_quiz/TakeSharedQuizPresenter.java
+++ src/main/java/interface_adapter/take_shared_quiz/TakeSharedQuizPresenter.java
@@ -1,10 +1,48 @@
 package interface_adapter.take_shared_quiz;
 
 import interface_adapter.ViewManagerModel;
+import interface_adapter.take_quiz.TakeQuizController;
 import use_cases.take_shared_quiz.TakeSharedQuizOutputBoundary;
 import use_cases.take_shared_quiz.TakeSharedQuizOutputData;
 
 public class TakeSharedQuizPresenter implements TakeSharedQuizOutputBoundary {
+
     private final TakeSharedQuizViewModel takeSharedQuizViewModel;
     private final ViewManagerModel viewManagerModel;
+    private TakeQuizController takeQuizController;
+
+    public TakeSharedQuizPresenter(TakeSharedQuizViewModel takeSharedQuizViewModel,
+                                   ViewManagerModel viewManagerModel) {
+        this.takeSharedQuizViewModel = takeSharedQuizViewModel;
+        this.viewManagerModel = viewManagerModel;
+    }
+
+    public void setTakeQuizController(TakeQuizController takeQuizController) {
+        this.takeQuizController = takeQuizController;
+    }
+
+    @Override
+    public void prepareSuccessView(TakeSharedQuizOutputData outputData) {
+        TakeSharedQuizState state = takeSharedQuizViewModel.getState();
+        state.setErrorMessage("");
+        takeSharedQuizViewModel.setState(state);
+
+        viewManagerModel.setState("take quiz");
+        viewManagerModel.firePropertyChange();
+
+        if (takeQuizController != null) {
+            takeQuizController.execute(outputData.getQuiz(), "");
+        }
+    }
+    @Override
+    public void prepareFailureView(String errorMessage) {
+
+        TakeSharedQuizState state = takeSharedQuizViewModel.getState();
+        state.setErrorMessage(errorMessage);
+        takeSharedQuizViewModel.setState(state);
+        takeSharedQuizViewModel.firePropertyChange();
+    }
 }
+
+
+
