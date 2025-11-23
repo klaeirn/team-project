af820945f1fb520d78cb48ce71eeaee7e7b19ec6
kelongrenrenren <liuyx1130@gmail.com>
2025-11-23 00:16:47 -0500

    take shared quiz use cases

---
+++ src/main/java/use_cases/take_shared_quiz/TakeSharedQuizInputData.java
@@ -0,0 +1,14 @@
+package use_cases.take_shared_quiz;
+
+
+public class TakeSharedQuizInputData {
+    private final String hash;
+
+    public TakeSharedQuizInputData(String hash) {
+        this.hash = hash;
+    }
+
+    public String getHash() {
+        return hash;
+    }
+}
