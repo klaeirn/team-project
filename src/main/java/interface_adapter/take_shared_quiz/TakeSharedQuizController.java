f15a978d9ef05eb9dad00322848549304ee704de
kelongrenrenren <liuyx1130@gmail.com>
2025-12-02 06:37:38 -0500

    missed on checkstyle problem and is now fixed

--- src/main/java/interface_adapter/take_shared_quiz/TakeSharedQuizController.java
+++ src/main/java/interface_adapter/take_shared_quiz/TakeSharedQuizController.java
@@ -16,11 +16,24 @@ public class TakeSharedQuizController {
         this.viewManagerModel = viewManagerModel;
     }
 
+    /**
+     * This method handles the user request to take a shared quiz
+     * Creates an input data object from the given hash and username
+     * and passes it to the use case interactor through the input boundary.
+     * @param hash     the shared quiz code entered by the user
+     * @param username the username of the current user
+     */
     public void execute(String hash, String username) {
         final TakeSharedQuizInputData inputData =
                 new TakeSharedQuizInputData(hash, username);
         takeSharedQuizInputBoundary.execute(inputData);
     }
+
+    /**
+     * Switches the UI to the Take Shared Quiz view
+     * Updates the view manager state and fires a property change so
+     * that the GUI can react and show the correct screen.
+     */
 
     public void switchToTakeSharedQuizView() {
         viewManagerModel.setState("take shared quiz");
